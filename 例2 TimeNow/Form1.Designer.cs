99b3b109054c057563b91c323bd6c17bc6be1d8c
ylixx <liyanem@qq.com>
2021-08-29 23:52:39 +0800

    添加例2

---
+++ 例2 TimeNow/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace 例2_TimeNow
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtMintue = new System.Windows.Forms.TextBox();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.txtSecon = new System.Windows.Forms.TextBox();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("隶书", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "冬奥会倒计时";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(46, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 73);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(9, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "冬奥会开幕事件：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(45, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "当前时间：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMintue);
+            this.groupBox2.Controls.Add(this.txtHour);
+            this.groupBox2.Controls.Add(this.txtSecon);
+            this.groupBox2.Controls.Add(this.txtDay);
+            this.groupBox2.Controls.Add(this.txtMonth);
+            this.groupBox2.Controls.Add(this.txtYear);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.ForeColor = System.Drawing.Color.Red;
+            this.groupBox2.Location = new System.Drawing.Point(29, 154);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(403, 125);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "距离冬奥会开幕还有";
+            this.groupBox2.UseCompatibleTextRendering = true;
+            // 
+            // txtMintue
+            // 
+            this.txtMintue.Location = new System.Drawing.Point(247, 52);
+            this.txtMintue.Name = "txtMintue";
+            this.txtMintue.Size = new System.Drawing.Size(100, 21);
+            this.txtMintue.TabIndex = 11;
+            // 
+            // txtHour
+            // 
+            this.txtHour.Location = new System.Drawing.Point(247, 25);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(100, 21);
+            this.txtHour.TabIndex = 10;
+            // 
+            // txtSecon
+            // 
+            this.txtSecon.Location = new System.Drawing.Point(247, 79);
+            this.txtSecon.Name = "txtSecon";
+            this.txtSecon.Size = new System.Drawing.Size(100, 21);
+            this.txtSecon.TabIndex = 9;
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(64, 80);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(100, 21);
+            this.txtDay.TabIndex = 8;
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(65, 53);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(100, 21);
+            this.txtMonth.TabIndex = 7;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(65, 25);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 21);
+            this.txtYear.TabIndex = 6;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label9.Location = new System.Drawing.Point(189, 86);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 12);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "秒：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label8.Location = new System.Drawing.Point(189, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "分：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label7.Location = new System.Drawing.Point(189, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "时：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.Location = new System.Drawing.Point(6, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "日：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(6, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "月：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(6, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "年：";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 291);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "使用Timer组件实现冬奥会倒计时";
+            this.Load += new System.EventHandler(this.Frm_Main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtMintue;
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.TextBox txtSecon;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 例2 TimeNow/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace 例2_TimeNow
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+        public long LogYear, LogMonth, LogDay, LogHour, LogMinte, LogSencon;
+
+        private void Frm_Main_Load(object sender, EventArgs e)
+        {
+            textBox2.Text ="2022/2/4 00:00:00"+" 星期五";
+            timer1.Enabled = true;//开启时间控件
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime GetTime = DateTime.Now;
+            DateTime StaOnTime = Convert.ToDateTime(Convert.ToDateTime("2022-2-4 00:00:00"));
+            txtYear.Text = DateAndTime.DateDiff(
+                "yyyy", GetTime, StaOnTime,
+                FirstDayOfWeek.Saturday,
+                FirstWeekOfYear.FirstFourDays).ToString();
+            txtMonth.Text = DateAndTime.DateDiff(
+                "m", GetTime, StaOnTime,
+                FirstDayOfWeek.Saturday,
+                FirstWeekOfYear.FirstFourDays).ToString();
+            txtDay.Text = DateAndTime.DateDiff(
+                "d", GetTime, StaOnTime,
+                FirstDayOfWeek.Saturday,
+                FirstWeekOfYear.FirstFourDays).ToString();
+            txtHour.Text = DateAndTime.DateDiff(
+                "H", GetTime, StaOnTime,
+                FirstDayOfWeek.Saturday,
+                FirstWeekOfYear.FirstFourDays).ToString();
+            txtMintue.Text = DateAndTime.DateDiff(
+                "m", GetTime, StaOnTime,
+                FirstDayOfWeek.Saturday,
+                FirstWeekOfYear.FirstFourDays).ToString();
+            txtSecon.Text = DateAndTime.DateDiff(
+                "s", GetTime, StaOnTime,
+                FirstDayOfWeek.Saturday,
+                FirstWeekOfYear.FirstFourDays).ToString();
+
+            textBox1.Text = DateTime.Now.ToString();
+        }
+
+    }
+
+}

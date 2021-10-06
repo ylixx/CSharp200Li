c9cb1261bd5d7801fb93a4d79141d08acc4be3a2
ylixx <liyanem@qq.com>
2021-10-06 23:21:54 +0800

    完成ping例子

---
+++ Ping测试/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Ping测试
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnt_Ping = new System.Windows.Forms.Button();
+            this.lst_PingResult = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bnt_Ping
+            // 
+            this.bnt_Ping.Location = new System.Drawing.Point(64, 274);
+            this.bnt_Ping.Name = "bnt_Ping";
+            this.bnt_Ping.Size = new System.Drawing.Size(75, 23);
+            this.bnt_Ping.TabIndex = 0;
+            this.bnt_Ping.Text = "测试";
+            this.bnt_Ping.UseVisualStyleBackColor = true;
+            this.bnt_Ping.Click += new System.EventHandler(this.bnt_Ping_Click);
+            // 
+            // lst_PingResult
+            // 
+            this.lst_PingResult.FormattingEnabled = true;
+            this.lst_PingResult.ItemHeight = 12;
+            this.lst_PingResult.Location = new System.Drawing.Point(22, 118);
+            this.lst_PingResult.Name = "lst_PingResult";
+            this.lst_PingResult.Size = new System.Drawing.Size(175, 136);
+            this.lst_PingResult.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "测试IP：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "测试结果：";
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(24, 55);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(173, 21);
+            this.txt_IP.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 316);
+            this.Controls.Add(this.txt_IP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lst_PingResult);
+            this.Controls.Add(this.bnt_Ping);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnt_Ping;
+        private System.Windows.Forms.ListBox lst_PingResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_IP;
+    }
+}
+
---
+++ Ping测试/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+namespace Ping测试
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bnt_Ping_Click(object sender, EventArgs e)
+        {
+            //清空列表框信息
+            this.lst_PingResult.Items.Clear();
+            //清空多余字符并赋值ipStr
+            string ipStr = txt_IP.Text.ToString().Trim();
+            //创建ping实例
+            Ping pingSender = new Ping();
+            //ping 选项设置
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+            int timeout = 120;
+            //测试数据
+            string data = "xxx";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            //调用send方法发送数据，将返回结果保存至pingReply实例
+            PingReply reply = pingSender.Send(ipStr, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                lst_PingResult.Items.Add("答复的主机地址：" + reply.Address.ToString());
+                lst_PingResult.Items.Add("往返时间：" + reply.RoundtripTime);
+                lst_PingResult.Items.Add("生存时间TTL：" + reply.Options.Ttl);
+                lst_PingResult.Items.Add("是否控制数据包的分段：" + reply.Options.DontFragment);
+                lst_PingResult.Items.Add("缓冲区大小：" + reply.Buffer.Length);
+            }
+            else
+            {
+                lst_PingResult.Items.Add(reply.Status.ToString());
+            }
+        }
+    }
+}
